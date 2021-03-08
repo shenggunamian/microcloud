3609eb23a0b0a401e96fbf5a89721b01b9d5a56d
shengguanmian <shengguanmian@163.com>
2021-03-08 17:30:54 +0800

    初始化项目

---
+++ microcloud-eureka/src/main/java/cn/enjoy/MicrocloudEurekaApplication.java
@@ -0,0 +1,13 @@
+package cn.enjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrocloudEurekaApplication.class, args);
+    }
+
+}
---
+++ microcloud-eureka/src/test/java/cn/enjoy/MicrocloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.enjoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrocloudEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
